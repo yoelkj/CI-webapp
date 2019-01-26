171bdd07cdb92157906bc794b6f05e818cc1f254
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2019-01-26 02:30:20 -0500

    Initial project, hello world ;)

---
+++ application/views/welcome_message.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello World - CI</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Yoel Velasquez Valencia</h1>
+	<p>Email: Yoel.velasquez.valencia@gmail.com</p>
+	<p>Skype: yoelkj</p>
+	<p>Website: velcodi.com</p>
+</div>
+
+</body>
+</html>
